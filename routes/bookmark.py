6fa8c5677451c38188f7039ab100ed7a09155ab4
yuvarajucet <yuvarajucet@gmail.com>
2022-11-28 22:44:21 +0530

    get all category for user

--- routes/bookmark.py
+++ routes/bookmark.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter,Depends,Body
+from fastapi import APIRouter,Depends,Body,Request
 from schemas.bookmark import createCategorySchema,newBookmarkSchema,editBookMarkSchema,deleteBookmarkSchema,deleteCategorySchema
 from auth.athenticateUser import JWTBearer
 from helper.boomarkHelper import generateCategoryId,generateBookmarId
-from dbController.bookmarkDBController import createBookmarkCategory,createNewBookmark,updateBookmark,deleteBookmarkData,deleteUserCategory
+from dbController.bookmarkDBController import createBookmarkCategory,createNewBookmark,updateBookmark,deleteBookmarkData,deleteUserCategory,getAlluserCategory
 
 bookmark = APIRouter(
     prefix="/api/v1/bookmark",
@@ -30,11 +30,11 @@ async def deleteBookmarkDa(deleteBookmark:deleteBookmarkSchema):
     response  = deleteBookmarkData(deleteBookmark)
     return response
 
-
 # bookmark category routers
 @bookmark.get("/getallcategory",dependencies=[Depends(JWTBearer())],tags=['bookmark'])
-async def getAllCategory():
-    pass
+async def getAllCategory(request:Request):
+    response = getAlluserCategory(request)
+    return response
 
 @bookmark.post("/createcategory",dependencies=[Depends(JWTBearer())],tags=['bookmark'])
 async def createCategory(categoryData:createCategorySchema = Body(...)):
