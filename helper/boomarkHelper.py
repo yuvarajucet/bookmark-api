6a8b92f6b7f433ea32d973d1aaed7a337ac5c642
yuvarajucet <yuvarajucet@gmail.com>
2022-12-06 22:33:57 +0530

    Favicon download functionality

--- helper/boomarkHelper.py
+++ helper/boomarkHelper.py
@@ -1,7 +1,20 @@
 import uuid
+import base64
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+from urllib.parse import urlparse
 
 def generateCategoryId():
     return str(uuid.uuid4())
 
 def generateBookmarId():
     return str(uuid.uuid4())
+
+def downloadWebsiteFavIcon(url):
+    # remove unwanted warning message
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+    favIconPath = '/favicon.ico'
+    domain = urlparse(url).netloc
+    image = requests.get('https://'+domain+favIconPath,verify=False).content
+    baseValue = base64.b64encode(image)
+    return baseValue
