487631f94170f9a49c32bbdc763078d7f44ceb6d
yuvarajucet <yuvarajucet@gmail.com>
2022-12-18 18:21:58 +0530

    changed email content

--- helper/emailHelper.py
+++ helper/emailHelper.py
@@ -70,41 +70,136 @@ def sendForgetEmailToUser(userData:userForgetPasswordSchema):
 def generateForgetPasswordEmailTemplate(url):
     return '''
     <html>
-    <body>
-        <div class="container" style="position:relative;top:0px;left:50%;transform:translate(-50%,0);background-color:rgb(255,255,255);width:90%;height:90%;text-align:center;box-shadow: 5px 5px 5px rgb(44, 44, 44);">
-            <div class="header-text">
-                <span style="font-size:30px">Hi</span>,
-                forget your password
-            </div>
-            <div class="button" style="position:relative;left:50%;top:0px;transform:translate(-50%,0);color:white;background-color:blueviolet;width:80px;text-align:center;padding:10px;font-size:20px;border-radius:10px">
-                <a href="{0}">forget password</a>
-            </div>
-            if you don't see button please click here <a style="color:aliceblue;text-decoration:none" href="{1}">click here</a>
-            <div class="info">
-                if you don't do this request please secure your account.
-            </div>
-        </div>
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <img width="60" src="#" title="logo" alt="logo">
+                          </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                            requested to reset your password</h1>
+                                        <span
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                            We cannot simply send you your old password. A unique link to reset your
+                                            password has been generated for you. To reset your password, click the button to verify
+                                        </p>
+                                        <a href="{0}"
+                                            style="background:#6862d5;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+                                            Password</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.bookmarking.com</strong></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        </table>
     </body>
     </html>
-    '''.format(url,url)
+    '''.format(url)
 
 def generateVerifyUserAccountHTMLEmailTemplate(url,username):
     return '''
     <html>
-    <body>
-        <div class="container" style="position:relative;top:0px;left:50%;transform:translate(-50%,0);background-color:rgb(255,255,255);width:90%;height:90%;text-align:center;box-shadow: 5px 5px 5px rgb(44, 44, 44);">
-            <div class="header-text">
-                <span style="font-size:30px">Hi</span> {0},
-                welcome to bookmarking please verify your account.
-            </div>
-            <div class="button" style="position:relative;left:50%;top:0px;transform:translate(-50%,0);color:white;background-color:blueviolet;width:80px;text-align:center;padding:10px;font-size:20px;border-radius:10px">
-                <a href="{1}">verify</a>
-            </div>
-            if you don't see button please click here <a style="color:aliceblue;text-decoration:none" href="{2}">click here</a>
-        </div>
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <img width="60" src="#" title="logo" alt="logo">
+                          </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Please verify your account</h1>
+                                        <span
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                           Hi {0} ,Please click the below button to verify your user account.
+                                        </p>
+                                        <a href="{1}"
+                                            style="background:#6862d5;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">
+                                            Verify</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.bookmarking.com</strong></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        </table>
     </body>
     </html>
-    '''.format(username,url,url)
+    '''.format(username,url)
 
 
 def sendEmailToUser(userEmail,htmlTemplate,subject):
